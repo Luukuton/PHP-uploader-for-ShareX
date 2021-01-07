24e8f30b4631bcdb4c7c952f125f6a26b0063e07
Yli-Hallila Aaron <45145923+yli-hallila@users.noreply.github.com>
2021-01-07 19:34:29 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,48 +1,40 @@
 <?php
-$config = include('config.php');
-$key = $config['secure_key'];
-$sharexdir = $config['sharexdir'];
-$domain_url = $config['domain_url'];
-$lengthofstring = $config['lengthofstring'];
 
-function RandomString($length) {
-    $keys = array_merge(range(0,9), range('a', 'z'));
+require_once("config.php");
 
-    for($i=0; $i < $length; $i++) {
-        $key .= $keys[mt_rand(0, count($keys) - 1)];
+if (isset($_POST["secret"]) && $_POST["secret"] == SECURE_KEY) {
+    $folderPath = SHAREX_DIR . format(FOLDER_NAME_FORMAT);
+    $fileName = format(FILE_NAME_FORMAT);
+    $fileType = pathinfo($_FILES["sharex"]["name"], PATHINFO_EXTENSION);
+    $filePath = $folderPath . "/" . $fileName . "." . $fileType;
+
+    if (!file_exists($folderPath)) {
+        $success = mkdir($folderPath, 0755);
+
+        if (!$success) {
+            die("Error creating folder: " . $folderPath);
+        }
     }
-    return $key;
+
+    if (move_uploaded_file($_FILES["sharex"]["tmp_name"], $filePath)) {
+        exit(DOMAIN . $filePath);
+    } else {
+        exit("File upload has failed");
+    }
+} else {
+    exit("No post data was received or secret key was invalid");
 }
 
-if ($_SERVER["REQUEST_URI"] == "/robot.txt") { die("User-agent: *\nDisallow: /"); }
+function format($str) {
+    $replace = [
+        "{day}"    => date("d"),
+        "{month}"  => date("m"),
+        "{year}"   => date("Y"),
+        "{hour}"   => date("H"),
+        "{minute}" => date("i"),
+        "{second}" => date("s"),
+        "{uniqid}" => substr(bin2hex(openssl_random_pseudo_bytes(16)), 0, FILENAME_LENGTH)
+    ];
 
-$year = date("Y");
-$month = date("m");
-$day = date("d");
-$hour = date("H");
-$minute = date("i");
-$filedate = $day."_".$hour.$minute;
-$foldername = $year."-".$month;
-
-if(file_exists($foldername)){echo 'Folder already exists. Uploading... ';}
-else{
-    mkdir($sharexdir.$foldername,0755);
-}
-
-if (isset($_POST['secret']) && $_POST['secret'] == $key) {
-    $filename = $filedate."-".RandomString($lengthofstring);
-    $target_file = $_FILES["sharex"]["name"];
-    $fileType = pathinfo($target_file, PATHINFO_EXTENSION);
-    if (move_uploaded_file($_FILES["sharex"]["tmp_name"], $sharexdir.$foldername."/".$filename.'.'.$fileType))
-    {
-        echo $domain_url.$sharexdir.$foldername."/".$filename.'.'.$fileType;
-    }
-        else
-    {
-       echo 'File upload has failed';
-    }
-}
-else
-{
-    echo 'Secret key was invalid or no post data was received';
+    return str_ireplace(array_keys($replace), array_values($replace), $str);
 }
